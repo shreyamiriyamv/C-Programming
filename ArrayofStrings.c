e6b2a0603cd870bfa76ad20b8226a454be80df2d
ShreyaMV <shreyamiriyamv@gmail.com>
2026-08-02 00:37:44 +0400

    Array of Strings

---
+++ ArrayofStrings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    char fruits [][10] = {"Apple",
+                          "Banana",
+                          "Coconut"};
+
+    int size = sizeof(fruits)/sizeof(fruits[0]);
+
+    for(int i = 0; i < size; i++) {
+        printf("%s ", fruits[i]);
+    }
+
+    return 0;
+}
