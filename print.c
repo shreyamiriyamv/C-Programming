b58769de672795f305a09c49e7e08c2bcce911f3
ShreyaMV <shreyamiriyamv@gmail.com>
2026-08-01 20:06:30 +0400

    Using printf and comments

---
+++ print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+    // This is a single line comment
+    printf("Hello. ");
+    printf("My name is Shreya Miriyam Varghese.\n");
+    printf("I am 20 years old.");
+    /* 
+    This 
+    is 
+    a 
+    multi-line
+    comment
+    */
+    return 0;
+}
